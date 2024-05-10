2fa889b430674f490cc4928f6981b66f7f5b226d
eden reuven <edenmatisr@gmail.com>
2024-05-10 16:30:12 +0300

    Add question 7

---
+++ Question7/Question7/NumericalExpression.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question7
+{
+    class NumericalExpression
+    {
+        private int number;
+        public NumericalExpression(int Number)
+        {
+            this.number = Number;
+        }
+
+        public int GetValue()
+        {
+            return number;
+        }
+        public override string ToString()
+        {
+            string numberToWord = "The number is: ";
+            if (number >= 1000000000)
+            {
+                numberToWord += NumberToString(number / 1000000000) + " billion ";
+                number = number % 1000000000;
+
+            }
+
+            if (number >= 1000000)
+            {
+                numberToWord += NumberToString(number / 1000000) + " million ";
+                number = number % 1000000;
+
+            }
+
+            if (number >= 1000)
+            {
+                numberToWord += NumberToString(number / 1000) + " thousand ";
+                number = number % 1000;
+
+            }
+
+            if (number < 1000)
+            {
+                numberToWord += NumberToString(number);
+
+            }
+
+            return (numberToWord);
+        }
+
+        static string NumberToString(int number)
+        {
+            string numberToWord = "";
+            string[] oneToNinty = { "","one", "two", "three", "four", "five", "six","seven", "eight", "nine", "ten",
+                "eleven","twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+            string[] tenths = { "", "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+            if (number >= 100)
+            {
+                numberToWord += oneToNinty[number / 100] + " hundred ";
+                number = number % 100;
+
+                if (number / 10 != 0)
+                {
+                    numberToWord += "and ";
+                }
+            }
+
+            if (number >= 20)
+            {
+
+                numberToWord += tenths[number / 10] + " ";
+
+                number = number % 10;
+
+            }
+
+            if (number >= 1)
+            {
+                numberToWord += oneToNinty[number];
+            }
+            return (numberToWord);
+        }
+
+        public static int SumLetters(int upToNumber)
+        {
+            int numberCount = 0;
+            int countLetters = 0;
+            int countTab = 0;
+            string numberToString = "";
+            while (numberCount < upToNumber)
+            {
+                numberToString = upToNumber.ToString();
+                countLetters = numberToString.Length;
+                for (int i = 0; i < numberToString.Length; i++)
+                {
+                    if (numberToString[i] == ' ')
+                        countTab++;
+                }
+                countLetters -= countTab;
+                numberCount++;
+            }
+            return countLetters;
+        }
+    }
+}
---
+++ Question7/Question7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumericalExpression num = new NumericalExpression(2);
+            int sumLetter = NumericalExpression.SumLetters(2);
+            Console.WriteLine(num.GetValue());
+            Console.WriteLine(num.ToString());
+            Console.WriteLine(sumLetter);
+
+        }
+    }
+}
